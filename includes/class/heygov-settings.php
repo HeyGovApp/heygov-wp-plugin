d1e1721113c6c2a257266aca8da71e8701660e30
SanjaBudic <sanja@hejdev.com>
2022-06-06 17:01:34 +0200

    Adding html code to diplay muni forms

--- includes/class/heygov-settings.php
+++ includes/class/heygov-settings.php
@@ -32,7 +32,26 @@ class HeyGovSettings {
 
 
     function heygov_forms_shortcode() {
-        return "Hello from the heygov forms shortcode";
+        $forms = wp_remote_get('https://api.heygov.com/heyville.org/forms');
+            if (is_wp_error($forms)) {
+                return $forms;
+            }
+            $forms = wp_remote_retrieve_body($forms);
+            $forms = json_decode($forms);
+        
+            $result = '<div class="row align-items-center">';
+                $result  .= '<div class="card">'; 
+                    $result .= '<div class="card-body p-3">';
+                        $result .= '<div class="row row-cols row-cols-2 row-cols-md-3 row-cols-lg-4 mb-3 d-flex">'; 
+                            foreach($forms as $form ) {
+                                $result .= '<div class="col my-2">'; 
+                                    $result .= '<a href="#" class="card card-form text-dark ratio ratio-1x1">' . $form->name . '</a>';
+                                $result .= '</div>';
+                            }
+                    $result .= '</div>'; 
+                $result .= '</div>';
+            $result .= '</div>'; 
+        return $result; 
     }
 
 }
